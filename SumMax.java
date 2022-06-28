66389489101f39755d59fa2014731eca957b79f6
IkedaRyoto <108253459+IkedaRyoto@users.noreply.github.com>
2022-06-28 10:43:33 +0900

    Add files via upload
    コメント追加

--- SumMax.java
+++ SumMax.java
@@ -10,7 +10,7 @@ public class SumMax {
 		
 		//投入金額が上限金額を越していないかの判定
 		if(sum <= 1990) { //  or  sum >= 2000
-			//1990円以下ならfalse
+			//1990円以下（2000円未満）ならfalse
 			summax = false;
 		}else {
 			//それ以外（1990円を超えた）ならtrue
