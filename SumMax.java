e2ee843c1fda0b9f19ab80d22fb996236d79463c
IkedaRyoto <108253459+IkedaRyoto@users.noreply.github.com>
2022-06-28 10:41:30 +0900

    Add files via upload

---
+++ SumMax.java
@@ -0,0 +1,23 @@
+
+public class SumMax {
+
+	public boolean SumMaxMethod(int sum){
+		
+		System.out.println("投入金額合計");
+		System.out.println(sum);
+		// boolean型で配列を宣言
+		boolean summax ;
+		
+		//投入金額が上限金額を越していないかの判定
+		if(sum <= 1990) { //  or  sum >= 2000
+			//1990円以下ならfalse
+			summax = false;
+		}else {
+			//それ以外（1990円を超えた）ならtrue
+			summax = true;
+		}
+		
+	return summax;	
+	}
+
+}
